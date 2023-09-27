e727c79009d3edfa1013e63214a5530349d49f44
cameron.adams <camgadams@gmail.com>
2023-09-27 18:38:09 +0200

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,121 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, ChangeEvent, useEffect } from "react";
+import { format } from "date-fns";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function formatt(value: string) {
+  const today = new Date();
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    format(today, "yyyy-MM-dd_HH-mm-ss_") + value.replace(/\s/g, "_") + ".xml"
+  );
 }
+
+function App() {
+  const [filename, setFilename] = useState<string>("");
+  const [fileContent, setFileContent] = useState<string>("");
+
+  const [copiedFileName, setCopiedFileName] = useState<boolean>();
+  const [copiedFileContent, setCopiedFileContent] = useState<boolean>();
+  const [downloadedFile, setDownLoadedFile] = useState<boolean>();
+
+  const handleCopyFilename = () => {
+    navigator.clipboard.writeText(filename).then(() => {
+      setCopiedFileName(true);
+    });
+  };
+
+  const handleCopyFileContent = () => {
+    navigator.clipboard.writeText(fileContent).then(() => {
+      setCopiedFileContent(true);
+    });
+  };
+
+  const handleCopyToXml = () => {
+    const blob = new Blob([fileContent], { type: "application/xml" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+    setDownLoadedFile(true);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newFilename = formatt(e.target.value);
+    setFilename(newFilename);
+    setFileContent(`<databaseChangeLog logicalFilePath="${newFilename}"
+                    xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                    xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd"> 
+
+  <changeSet id="1" author="adminuser" runOnChange="true">
+    <preConditions onFail="MARK_RAN">
+      <sqlCheck expectedResult="0">
+      </sqlCheck>
+    </preConditions>
+    <sql splitStatements="true">  
+    </sql>
+  </changeSet>
+</databaseChangeLog>
+    `);
+  };
+
+  return (
+    <div className="bg-slate-400">
+      <div className="flex flex-col mx-auto w-4/5 h-screen  p-16 gap-6">
+        <div className="flex justify-between">
+          <input
+            className="p-4 w-1/2 h-10 border-2 border-gray-300 rounded-md"
+            type="text"
+            placeholder="Enter what the patch does: e.g. add task to notify admin users"
+            onChange={handleInputChange}
+          />
+          {fileContent && (
+            <button
+              className="bg-yellow-100 rounded-md px-2"
+              onClick={handleCopyToXml}
+            >
+              {downloadedFile ? "Downloaded ✅" : "Download File"}
+            </button>
+          )}
+        </div>
+
+        {fileContent && (
+          <>
+            <div className="bg-slate-200 p-4 rounded-lg">
+              <div className="flex justify-between mb-4">
+                <p>Filename:</p>
+                <button
+                  className="bg-yellow-100 rounded-md px-2"
+                  onClick={handleCopyFilename}
+                >
+                  {copiedFileName ? "Copied ✅" : "Copy File Name"}
+                </button>
+              </div>
+
+              <pre className="bg-slate-300 p-4 rounded-lg">{filename}</pre>
+            </div>
+            {/* {copySuccess && <p className="text-green-500">{copySuccess}</p>} */}
+
+            {/* <button onClick={handleCopyFilename}>Copy Filename</button> */}
+
+            <div className="bg-slate-200 p-4 rounded-lg">
+              <div className="flex justify-between mb-4">
+                <p>File Content:</p>
+                <button
+                  className="bg-yellow-100 rounded-md px-2"
+                  onClick={handleCopyFileContent}
+                >
+                  {copiedFileContent ? "Copied ✅" : "Copy File Content"}
+                </button>
+              </div>
+
+              <pre className="bg-slate-300 p-4 rounded-lg">{fileContent}</pre>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
